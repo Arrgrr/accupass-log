5745ce8af0f1e630e70cfe56699b6d27adaefd07
Matt Chang <mattchang@accuvally.com>
2014-09-11 18:02:27 +0800

    add log models

--- log/models.py
+++ log/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Log(models.Model):
+	user = models.ForeignKey(u'User', blank=True, null=True)
+	error_type = models.ForeignKey(u'Category', blank=True, null=True)
+	message = models.CharField(u'Message', max_length=200)
+	log_date = models.DateTimeField('date published')
+
+	def __unicode__(self):
+		return self.message
+
+class User(models.Model):
+	name = models.CharField(u'Name', max_length=50)
+	email = models.CharField(u'Email', max_length=50)
+	userid = models.CharField(u'User_ID', max_length=50)
+
+	def __unicode__(self):
+		return self.name
+
+class Category(models.Model):
+	title = models.CharField(u'Title', max_length=50)
+
+	def __unicode__(self):
+		return self.title
