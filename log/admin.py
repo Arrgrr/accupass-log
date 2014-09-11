b6319427c0cb3a30548fd841871076d85d6c48c2
Matt Chang <mattchang@accuvally.com>
2014-09-11 18:05:07 +0800

    add admin

--- log/admin.py
+++ log/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from log.models import Log, Category, User
 
-# Register your models here.
+
+admin.site.register(Log)
+admin.site.register(User)
+admin.site.register(Category)
